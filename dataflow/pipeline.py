632b72672598be7f20991b302e549456348ba90d
saikiranreven <rsksislvgy@gmail.com>
2025-07-15 22:35:49 -0230

    tfy

--- dataflow/pipeline.py
+++ dataflow/pipeline.py
@@ -1,5 +1,6 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.window import FixedWindows
 import json
 
 def run():
@@ -9,10 +10,15 @@ def run():
     )
 
     with beam.Pipeline(options=options) as p:
+        # Simple streaming pipeline without GroupByKey
         (p
-         | beam.io.ReadFromPubSub(topic='projects/bct-project-465419/topics/stream-topic')
-         | beam.Map(lambda x: json.loads(x.decode('utf-8')))
-         | beam.io.WriteToText('gs://YOUR_BUCKET/raw/events')
+         | 'ReadFromPubSub' >> beam.io.ReadFromPubSub(
+             topic='projects/YOUR_PROJECT/topics/stream-topic')
+         | 'WindowInto' >> beam.WindowInto(FixedWindows(60))  # Critical fix
+         | 'ParseJSON' >> beam.Map(lambda x: json.loads(x.decode('utf-8')))
+         | 'WriteToGCS' >> beam.io.WriteToText(
+             'gs://YOUR_BUCKET/raw/events',
+             file_name_suffix='.json')
         )
 
 if __name__ == '__main__':
